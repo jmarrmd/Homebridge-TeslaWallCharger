fb6c4750504d510d52be3c5f49be4d6b5ff5eab0
jmarrmd <91326068+jmarrmd@users.noreply.github.com>
2026-03-29 14:48:51 -0600

    Delete dist directory

--- dist/settings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PLUGIN_NAME = exports.PLATFORM_NAME = void 0;
-/**
- * Global constants used throughout the plugin.
- */
-exports.PLATFORM_NAME = 'TeslaWallConnector';
-exports.PLUGIN_NAME = 'homebridge-tesla-wall-connector';
-//# sourceMappingURL=settings.js.map
